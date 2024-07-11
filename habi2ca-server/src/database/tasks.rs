db3aa303180e2062fada33591d575f3a9b173528
Albert Garde <albertsgarde@gmail.com>
2024-07-11 12:54:56 +0200

    :sparkles: Add get tasks endpoint

--- habi2ca-server/src/database/tasks.rs
+++ habi2ca-server/src/database/tasks.rs
@@ -1,6 +1,7 @@
 use anyhow::bail;
 use anyhow::Context;
 use anyhow::Result;
+use habi2ca_common::player::PlayerId;
 use habi2ca_common::task::{Task, TaskData, TaskId};
 use tokio_rusqlite::OptionalExtension;
 
@@ -24,6 +25,59 @@ impl Database {
                     ))
                     .with_context(move || format!("Failed to insert row for new task {task:?}."))
                     .map(TaskId)
+            })
+            .await
+    }
+
+    pub async fn get_tasks(&self) -> Result<Vec<Task>> {
+        const GET_TASKS: &str = "SELECT id, player_id, name, description, completed FROM task";
+
+        self.connection
+            .call_unwrap(move |connection| {
+                let mut statement = connection.prepare(GET_TASKS)?;
+                let tasks = statement
+                    .query_map((), |row| {
+                        Ok(Task {
+                            id: row.get(0)?,
+                            data: TaskData {
+                                player: row.get(1)?,
+                                name: row.get(2)?,
+                                description: row.get(3)?,
+                                completed: row.get(4)?,
+                            },
+                        })
+                    })
+                    .context("SQLite call to get tasks failed.")?
+                    .collect::<Result<_, _>>()
+                    .context("Failed to construct task from DB row.")?;
+                Ok(tasks)
+            })
+            .await
+    }
+
+    pub async fn get_player_tasks(&self, player_id: PlayerId) -> Result<Vec<Task>> {
+        const GET_PLAYER_TASKS: &str =
+            "SELECT id, name, description, completed FROM task WHERE player_id = ?1";
+
+        self.connection
+            .call_unwrap(move |connection| {
+                let mut statement = connection.prepare(GET_PLAYER_TASKS)?;
+                let tasks = statement
+                    .query_map((player_id,), |row| {
+                        Ok(Task {
+                            id: row.get(0)?,
+                            data: TaskData {
+                                player: player_id,
+                                name: row.get(1)?,
+                                description: row.get(2)?,
+                                completed: row.get(3)?,
+                            },
+                        })
+                    })
+                    .context("SQLite call to get tasks failed.")?
+                    .collect::<Result<_, _>>()
+                    .context("Failed to construct task from DB row.")?;
+                Ok(tasks)
             })
             .await
     }
--- habi2ca-server/src/routes/tasks.rs
+++ habi2ca-server/src/routes/tasks.rs
@@ -1,10 +1,12 @@
+use std::collections::HashMap;
+
 use actix_web::{
     get, patch, post,
     web::{self, Json},
     HttpRequest, Responder, Scope,
 };
-use anyhow::Context;
-use habi2ca_common::task::TaskData;
+use anyhow::{anyhow, Context};
+use habi2ca_common::{player::PlayerId, task::TaskData};
 
 use crate::{routes::RouteError, state::State};
 
@@ -14,15 +16,49 @@ pub async fn create_task(
     task: Json<TaskData>,
 ) -> Result<impl Responder, RouteError> {
     let task_data = task.into_inner();
-    println!("Creating task with name {}.", task_data.name());
     let task_id = state.database().create_task(task_data.clone()).await?;
     let task = state
         .database()
         .get_task(task_id)
         .await?
         .context("Failed to get newly created task.")?;
-    assert_eq!(task.data, task_data);
+    if task.data != task_data {
+        Err(anyhow!(
+            "Task data mismatch: {:?} != {task_data:?}.",
+            task.data
+        ))?;
+    }
     Ok(web::Json(task))
+}
+
+#[get("")]
+pub async fn get_tasks(
+    state: web::Data<State>,
+    query: web::Query<HashMap<String, String>>,
+) -> Result<impl Responder, RouteError> {
+    let player_id = query
+        .get("player")
+        .map(|s| {
+            s.parse()
+                .with_context(|| format!("Failed to parse player id '{s}'."))
+                .map(PlayerId)
+        })
+        .transpose()?;
+    if let Some(player_id) = player_id {
+        let tasks = state
+            .database()
+            .get_player_tasks(player_id)
+            .await
+            .with_context(|| format!("Failed to get tasks for player {player_id}."))?;
+        Ok(web::Json(tasks))
+    } else {
+        let tasks = state
+            .database()
+            .get_tasks()
+            .await
+            .context("Failed to get tasks.")?;
+        Ok(web::Json(tasks))
+    }
 }
 
 #[get("/{id}")]
@@ -71,8 +107,9 @@ pub async fn complete_task(
 
 pub fn add_routes(scope: Scope) -> Scope {
     scope
-        .service(get_task)
         .service(create_task)
+        .service(get_tasks)
+        .service(get_task)
         .service(complete_task)
 }
 
@@ -82,6 +119,69 @@ mod tests {
     use habi2ca_common::task::{Task, TaskData};
 
     use crate::{database::Database, start::create_app};
+
+    #[tokio::test]
+    async fn get_tasks() {
+        let database = Database::create_in_memory().await.unwrap();
+        let player_id = database.create_player("Alice").await.unwrap();
+        let task_id = database
+            .create_task(TaskData::new(player_id, "Task1", "Description1"))
+            .await
+            .unwrap();
+        let task_id2 = database
+            .create_task(TaskData::new(player_id, "Task2", "Description2"))
+            .await
+            .unwrap();
+
+        let app = test::init_service(create_app(database)).await;
+
+        let tasks: Vec<Task> =
+            test::call_and_read_body_json(&app, TestRequest::get().uri("/api/tasks").to_request())
+                .await;
+
+        assert_eq!(tasks.len(), 2);
+        assert_eq!(tasks[0].id, task_id);
+        assert_eq!(tasks[0].data.player, player_id);
+        assert_eq!(tasks[0].data.name, "Task1");
+        assert_eq!(tasks[0].data.description, "Description1");
+        assert_eq!(tasks[0].data.completed, false);
+
+        assert_eq!(tasks[1].id, task_id2);
+        assert_eq!(tasks[1].data.player, player_id);
+        assert_eq!(tasks[1].data.name, "Task2");
+        assert_eq!(tasks[1].data.description, "Description2");
+        assert_eq!(tasks[1].data.completed, false);
+    }
+
+    #[tokio::test]
+    async fn get_player_tasks() {
+        let database = Database::create_in_memory().await.unwrap();
+        let player_id = database.create_player("Alice").await.unwrap();
+        let player2_id = database.create_player("Bob").await.unwrap();
+        let _task_id = database
+            .create_task(TaskData::new(player2_id, "Task1", "Description1"))
+            .await
+            .unwrap();
+        let task_id2 = database
+            .create_task(TaskData::new(player_id, "Task2", "Description2"))
+            .await
+            .unwrap();
+
+        let app = test::init_service(create_app(database)).await;
+
+        let tasks: Vec<Task> = test::call_and_read_body_json(
+            &app,
+            TestRequest::get().uri("/api/tasks?player=1").to_request(),
+        )
+        .await;
+
+        assert_eq!(tasks.len(), 1);
+        assert_eq!(tasks[0].id, task_id2);
+        assert_eq!(tasks[0].data.player, player_id);
+        assert_eq!(tasks[0].data.name, "Task2");
+        assert_eq!(tasks[0].data.description, "Description2");
+        assert_eq!(tasks[0].data.completed, false);
+    }
 
     #[tokio::test]
     async fn create_task() {
