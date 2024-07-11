317086b6e4cc16033d69f7ff668c7daea9df3124
Albert Garde <albertsgarde@gmail.com>
2024-07-11 12:55:13 +0200

    :sparkles: Add basic task support to frontend

--- habi2ca-frontend/src/main.rs
+++ habi2ca-frontend/src/main.rs
@@ -1,17 +1,31 @@
-use habi2ca_common::player::Player;
+use habi2ca_common::{
+    player::Player,
+    task::{Task, TaskId},
+};
 use iced::{
     executor,
     futures::FutureExt,
-    widget::{button, column, row, text, Text},
+    widget::{button, column, row, text, Column, Text},
     Application, Command,
 };
-use reqwest::{Client, Url};
+use reqwest::{Client, Response, Url};
 use serde_json::json;
+
+async fn unwrap_response(response: Response) -> Response {
+    if response.status().is_success() {
+        response
+    } else {
+        panic!("Request failed: {}", response.text().await.unwrap())
+    }
+}
 
 #[derive(Debug, Clone)]
 pub enum Message {
-    ShowPlayer(Player),
+    UpdatePlayer(Player),
     AddXp,
+    CreateTask,
+    CompleteTask(TaskId),
+    UpdateTasks(Vec<Task>),
 }
 
 pub struct Config {
@@ -21,6 +35,7 @@ pub struct Config {
 pub struct App {
     server_url: Url,
     player: Option<Player>,
+    tasks: Vec<Task>,
     client: Client,
 }
 
@@ -36,18 +51,28 @@ impl Application for App {
     fn new(config: Config) -> (Self, Command<Message>) {
         let client = Client::new();
         let Config { server_url } = config;
-        let get_player = client
+        let update_player = client
             .get(server_url.join("api/players/1").unwrap())
             .send()
             .then(|response| response.unwrap().json())
-            .map(|player| Message::ShowPlayer(player.unwrap()));
+            .map(|player| Message::UpdatePlayer(player.unwrap()));
+        let tasks_url = server_url.join("api/tasks").unwrap();
+        let update_tasks = client
+            .get(tasks_url)
+            .send()
+            .then(|response| response.unwrap().error_for_status().unwrap().json())
+            .map(|tasks| Message::UpdateTasks(tasks.unwrap()));
         (
             Self {
                 server_url,
                 player: None,
+                tasks: vec![],
                 client,
             },
-            Command::perform(get_player, |message| message),
+            Command::batch([
+                Command::perform(update_player, |message| message),
+                Command::perform(update_tasks, |message| message),
+            ]),
         )
     }
 
@@ -71,11 +96,53 @@ impl Application for App {
                     }))
                     .send()
                     .then(move |response| response.unwrap().error_for_status().unwrap().json())
-                    .map(|player| Message::ShowPlayer(player.unwrap()));
+                    .map(|player| Message::UpdatePlayer(player.unwrap()));
                 Command::perform(update_xp, |message| message)
             }
-            Message::ShowPlayer(player) => {
+            Message::UpdatePlayer(player) => {
                 self.player = Some(player);
+                Command::none()
+            }
+            Message::CreateTask => {
+                let client = self.client.clone();
+                let tasks_url = self.server_url.join("api/tasks").unwrap();
+                let create_task = client
+                    .clone()
+                    .post(tasks_url.as_ref())
+                    .json(&json!({
+                        "player": 1,
+                        "name": "Task1",
+                        "description": "Description1",
+                        "completed": false,
+                    }))
+                    .send()
+                    .then(|response| unwrap_response(response.unwrap()))
+                    .then(|response| response.json::<Task>())
+                    .then(move |_task| client.get(tasks_url).send())
+                    .then(|response| response.unwrap().error_for_status().unwrap().json())
+                    .map(|tasks| Message::UpdateTasks(tasks.unwrap()));
+                Command::perform(create_task, |message| message)
+            }
+            Message::CompleteTask(task_id) => {
+                let client = self.client.clone();
+                let complete_task_url = self
+                    .server_url
+                    .join(&format!("api/tasks/{task_id}/complete"))
+                    .unwrap();
+                let tasks_url = self.server_url.join("api/tasks").unwrap();
+                let complete_task = client
+                    .clone()
+                    .patch(complete_task_url.as_ref())
+                    .send()
+                    .then(|response| unwrap_response(response.unwrap()))
+                    .then(|response| response.json::<Task>())
+                    .then(move |_task| client.get(tasks_url).send())
+                    .then(|response| response.unwrap().error_for_status().unwrap().json())
+                    .map(|tasks| Message::UpdateTasks(tasks.unwrap()));
+                Command::perform(complete_task, |message| message)
+            }
+            Message::UpdateTasks(tasks) => {
+                self.tasks = tasks;
                 Command::none()
             }
         }
@@ -83,13 +150,32 @@ impl Application for App {
 
     fn view(&self) -> iced::Element<'_, Self::Message> {
         if let Some(player) = &self.player {
-            column![
-                text(format!("id: {}", player.id)),
-                text(format!("Name: {}", player.data.name)),
-                row![
-                    text(format!("XP: {}", player.data.xp)),
-                    button("Add XP").on_press(Message::AddXp)
-                ]
+            let tasks = Column::with_children(
+                self.tasks
+                    .iter()
+                    .filter(|task| !task.data.completed())
+                    .flat_map(|task| {
+                        [
+                            text(format!("{}", task.data.name)).into(),
+                            text(format!("    {}", task.data.description)).into(),
+                            button("Complete")
+                                .on_press(Message::CompleteTask(task.id))
+                                .into(),
+                        ]
+                    }),
+            );
+
+            row![
+                column![
+                    text(format!("id: {}", player.id)),
+                    text(format!("Name: {}", player.data.name)),
+                    row![
+                        text(format!("XP: {}", player.data.xp)),
+                        button("Add XP").on_press(Message::AddXp)
+                    ],
+                    tasks,
+                ],
+                button("Create Task").on_press(Message::CreateTask)
             ]
             .into()
         } else {
