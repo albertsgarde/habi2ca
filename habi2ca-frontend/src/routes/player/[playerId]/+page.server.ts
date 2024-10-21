ef8d38f9f17d3c2ef9acf0d63eefb8b5094ab8fc
Albert Garde <albertsgarde@gmail.com>
2024-10-21 13:26:54 +0200

    :sparkles: Use route to specify player

--- habi2ca-frontend/src/routes/player/[playerId]/+page.server.ts
+++ habi2ca-frontend/src/routes/player/[playerId]/+page.server.ts
@@ -1,12 +1,21 @@
 
+
 import { BACKEND_ORIGIN } from '$lib/base';
 import type { Player } from '$lib/player';
 import type { Task } from '$lib/task';
 import { error } from '@sveltejs/kit';
 
-export async function load(): Promise<{ player: Player, tasks: Task[] }> {
-    const playerUrl = `${BACKEND_ORIGIN}/api/players/1`;
-    const tasksUrl = `${BACKEND_ORIGIN}/api/tasks?player=1`;
+export async function load({ params }: { params: { playerId: string } }): Promise<{ player: Player, tasks: Task[] }> {
+    const playerIdStr = params.playerId;
+
+    const playerId = parseInt(playerIdStr);
+
+    if (isNaN(playerId)) {
+        error(400, "Invalid player ID");
+    }
+
+    const playerUrl = `${BACKEND_ORIGIN}/api/players/${playerId}`;
+    const tasksUrl = `${BACKEND_ORIGIN}/api/tasks?player=${playerId}`;
     const playerPromise = fetch(playerUrl);
     const tasksPromise = fetch(tasksUrl);
 
