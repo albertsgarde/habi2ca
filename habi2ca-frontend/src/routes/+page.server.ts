8372cfda7b096d417b490540d2678c88e007a3f2
Albert Garde <albertsgarde@gmail.com>
2024-10-22 10:32:29 +0200

    :bug: Fix types

--- habi2ca-frontend/src/routes/+page.server.ts
+++ habi2ca-frontend/src/routes/+page.server.ts
@@ -3,17 +3,16 @@
 import { BACKEND_ORIGIN, fetchJson } from '$lib/base';
 import type { Player } from '$lib/player';
 import type { Task } from '$lib/task';
-import { error } from '@sveltejs/kit';
 import type { PlayerInfo } from './playerInfo';
 
 export async function load(): Promise<{ players: PlayerInfo[] }> {
-    const players = await fetchJson(`${BACKEND_ORIGIN}/api/players`, "Failed to fetch players");
+    const players: Player[] = await fetchJson(`${BACKEND_ORIGIN}/api/players`, "Failed to fetch players");
 
-    const playerPromises = players.map(async player =>  {
-        let task = await fetchJson(`${BACKEND_ORIGIN}/api/tasks?player=${player.id}`);
-        return {player: player, numTasks: task.length};
+    const playerPromises = players.map(async player => {
+        let tasks: Task[] = await fetchJson(`${BACKEND_ORIGIN}/api/tasks?player=${player.id}`, `Failed to get tasks for player ${player.id}`);
+        return { player: player, numTasks: tasks.length };
     }
     );
     const playerInfos = await Promise.all(playerPromises);
-    return {players: playerInfos};
+    return { players: playerInfos };
 }
