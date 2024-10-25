499ac1a3840d0237c04353dc20efbd4f1758d8b9
Albert Garde <albertsgarde@gmail.com>
2024-10-25 15:03:08 +0200

    :zap: Reduce time spent by pre-commit

--- habi2ca-ops/src/test.rs
+++ habi2ca-ops/src/test.rs
@@ -1,32 +1,76 @@
-use std::{path::Path, process::Command};
-
 use anyhow::{bail, Context, Result};
 use clap::Args;
+use std::{
+    path::{Path, PathBuf},
+    process::Command,
+};
 
 #[derive(Args, Debug, Clone)]
-pub struct Test {}
+pub struct Test {
+    /// Whether to fix issues
+    #[arg(short, long, default_value = "false")]
+    fix: bool,
+    #[arg(short, long, default_value = "false")]
+    quick: bool,
+    /// Input paths
+    paths: Vec<String>,
+}
 
 impl Test {
     pub fn run(&self, workspace_dir: impl AsRef<Path>) -> Result<()> {
         let workspace_dir = workspace_dir.as_ref();
 
+        let paths = self.paths.iter().map(PathBuf::from).collect::<Vec<_>>();
+        let frontend_paths: Vec<_> = paths
+            .iter()
+            .flat_map(|path| path.strip_prefix("habi2ca-frontend"))
+            .collect();
+
+        let rust_paths: Vec<_> = paths
+            .iter()
+            .filter(|path| path.extension().map_or(false, |ext| ext == "rs"))
+            .collect();
+
+        let skip_backend = !paths.is_empty() && frontend_paths.is_empty();
+
         let mut command = Command::new(env!("CARGO"));
-        command
-            .args(["clippy", "--all-targets", "--", "-Dwarnings"])
-            .current_dir(workspace_dir);
-        let status = command.spawn().context("Failed to run clippy.")?.wait()?;
+        if self.fix {
+            command.args([
+                "clippy",
+                "--no-deps",
+                "--fix",
+                "--allow-staged",
+                "--",
+                "-Dwarnings",
+            ]);
+        } else {
+            command.args(["clippy", "--no-deps", "--", "-Dwarnings"]);
+        }
+        let status = command
+            .current_dir(workspace_dir)
+            .spawn()
+            .context("Failed to run clippy.")?
+            .wait()?;
         if !status.success() {
             bail!("Clippy found warnings.");
         }
 
-        let mut command = Command::new(env!("CARGO"));
-        command.args(["fmt", "--check"]).current_dir(workspace_dir);
-        let status = command
-            .spawn()
-            .context("Failed to check formatting.")?
-            .wait()?;
-        if !status.success() {
-            bail!("Formatting incorrect.");
+        if !rust_paths.is_empty() {
+            let mut command = Command::new(env!("CARGO"));
+            if self.fix {
+                command.args(["fmt"]);
+            } else {
+                command.args(["fmt", "--check"]);
+            }
+            command.args(["--"]).args(rust_paths.as_slice());
+            let status = command
+                .current_dir(workspace_dir)
+                .spawn()
+                .context("Failed to check formatting.")?
+                .wait()?;
+            if !status.success() {
+                bail!("Formatting incorrect.");
+            }
         }
 
         let mut command = Command::new(env!("CARGO"));
@@ -36,17 +80,61 @@ impl Test {
             bail!("Some tests failed.");
         }
 
-        let mut command = Command::new("npm");
-        command
-            .args(["run", "check-all"])
-            .current_dir(workspace_dir.join("habi2ca-frontend"));
-        let status = command
-            .spawn()
-            .context("Failed to run frontend checks.")?
-            .wait()?;
-        if !status.success() {
-            bail!("Some frontend checks failed.");
+        // "check-all": "npm run check && npm run lint && npm run format && npm run test"
+
+        if !skip_backend {
+            let mut command = Command::new("npm");
+            let status = command
+                .args(["run", "check"])
+                .current_dir(workspace_dir.join("habi2ca-frontend"))
+                .spawn()?
+                .wait()?;
+            if !status.success() {
+                bail!("Frontend check failed.");
+            }
+
+            let mut command = Command::new("npm");
+            let status = command
+                .args(["run", "lint"])
+                .current_dir(workspace_dir.join("habi2ca-frontend"))
+                .spawn()?
+                .wait()?;
+            if !status.success() {
+                bail!("Frontend lint failed.");
+            }
+
+            let mut command = Command::new("npx");
+            if self.fix {
+                command.args(["prettier", "--write"]);
+            } else {
+                command.args(["prettier", "--check"]);
+            }
+            if frontend_paths.is_empty() {
+                command.arg(".");
+            } else {
+                command.args(frontend_paths.as_slice());
+            }
+            let status = command
+                .current_dir(workspace_dir.join("habi2ca-frontend"))
+                .spawn()?
+                .wait()?;
+            if !status.success() {
+                bail!("Frontend formatting incorrect.");
+            }
+
+            if !self.quick {
+                let mut command = Command::new("npm");
+                let status = command
+                    .args(["run", "test"])
+                    .current_dir(workspace_dir.join("habi2ca-frontend"))
+                    .spawn()?
+                    .wait()?;
+                if !status.success() {
+                    bail!("Frontend tests failed.");
+                }
+            }
         }
+
         Ok(())
     }
 }
